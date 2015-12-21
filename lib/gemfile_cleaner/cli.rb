d87b99ac5cedbca0a2d698a143642f11e68be537
totzYuta <yuta.totz@gmail.com>
2015-12-21 18:24:27 +0900

    Implementing read line and collect gem file names

--- lib/gemfile_cleaner/cli.rb
+++ lib/gemfile_cleaner/cli.rb
@@ -5,7 +5,24 @@ module GemfileCleaner
   class CLI < Thor
     desc "hello", "hello print"
     def hello
-      p "yes"
+      gems = Array.new
+      gems = read_gems
+    end
+
+    no_commands do
+      def read_gems
+        gems = Array.new
+        File.open("./lib/Gemfile", "r") do |file|
+          file.each_line do |line|
+            if line =~ /^gem */
+              # gems << line.sub(/gem /, "")
+              gems << line.sub(/^gem "/, "")
+            elsif line =~ /^group*/
+              # separate by each group
+            end
+          end
+        end
+      end
     end
   end
 end
